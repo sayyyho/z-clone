9b597ab49afe44d8fb2f83ceb3c012e60fc9f2d8
sayyyho <323psh@naver.com>
2022-07-10 23:33:31 +0900

    add ws server

--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
+
 const app = express();
 
 app.set('view engine', "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res)=> {
+app.get("/", (_, res)=> {
     res.render("home");
 })
-app.get("/*", (req,res)=>{
+app.get("/*", (_,res)=>{
     res.redirect("/");
 })
 
-console.log("hello");
-app.listen(3000);
+const server = http.createServer(app); 
+const wss = new WebSocket.Server({ server });
 
+server.listen(3000, ()=> console.log("Listening on http://localhost:3000"));
+// http and ws protocol share same port
